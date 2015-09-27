ee56f88d6fc6ebe28ebf031069a8979e505e7213
Junichi Tokuda <tokuda@bwh.harvard.edu>
2015-09-27 18:18:11 -0400

    Bug fix. It is now working with the experimental data.

--- TransformTargets.py
+++ TransformTargets.py
@@ -10,15 +10,23 @@ import csv
 from __main__ import vtk, qt, ctk, slicer
 from slicer.ScriptedLoadableModule import *
 
-path = '/Users/junichi/Dropbox/Experiments/BRP/BRPRobotCases/Scene'
-dataFile = 'RobotCase-Log.csv'
-
-
 # NOTE: The script assumes that there is a -1 offset between the image and transform indices,
 # because transforms are calculated from the previous images. (We used TSE for registration,
 # and VIBE for transform).
 
+# This script requires CurveMaker module to measure the minimum distance between points and curves.
+
+path = '/Users/junichi/Dropbox/Experiments/BRP/BRPRobotCases/Scene'
+dataFile = 'RobotCase-Log.csv'
+
 def main():
+
+    # Initialize CurveMaker module
+    slicer.util.selectModule('CurveMaker')
+    cmlogic = slicer.modules.CurveMakerWidget.logic
+    cmlogic.setInterpolationMethod(1)
+    cmlogic.setRing(0)
+    cmlogic.setTubeRadius(1.0)
 
     caseIndex = -1
     targetIndex = -1
@@ -33,13 +41,20 @@ def main():
                 caseIndex = row.index('Case')
                 targetIndex = row.index('Tgt')
                 imageIndex = row.index('MRI')
+                robotTgtRIndex = row.index('TgtR')
+                robotTgtAIndex = row.index('TgtA')
+                robotTgtSIndex = row.index('TgtS')
+
             else:
                 case = int(row[caseIndex])
                 target = int(row[targetIndex])
                 image = int(row[imageIndex])
-                caseData.append((case, target, image))
+                robotTgtR = float(row[robotTgtRIndex])
+                robotTgtA = float(row[robotTgtAIndex])
+                robotTgtS = float(row[robotTgtSIndex])
+                caseData.append((case, target, image, [robotTgtR, robotTgtA, robotTgtS]))
 
-    for (case, target, image) in caseData:
+    for (case, target, image, robotTgt) in caseData:
 
         print 'Processing for case=%d, image=%d...' % (case, image)
         
@@ -75,9 +90,51 @@ def main():
 
         transformedTargetFilePath = '%s/Case%03d/Targets-transformed-%02d.fcsv' % (path, case, image)
         slicer.util.saveNode(targetsNode, transformedTargetFilePath)
+
+        # Get actual biopsy target
+        nTargets = targetsNode.GetNumberOfFiducials()
+        if target > nTargets:
+            print 'The target index exceeds the largest index (%d / %d).' % (target, nTargets)
+            slicer.mrmlScene.RemoveNode(transformNode)
+            slicer.mrmlScene.RemoveNode(targetsNode)
+            continue
+
+        biopsyTgt = [0.0, 0.0, 0.0]
+        targetsNode.GetNthFiducialPosition(target-1, biopsyTgt)
         
+        # Load trajectory
+        trajectoryFilePath = '%s/Case%03d/Traj-%d.fcsv' % (path, case, image)
+        trajectoryNode = None
+        if os.path.isfile(trajectoryFilePath):
+            (r, trajectoryNode) = slicer.util.loadMarkupsFiducialList(trajectoryFilePath, True)
+        if not trajectoryNode:
+            print 'Could not find trajectory file.'
+            slicer.mrmlScene.RemoveNode(transformNode)
+            slicer.mrmlScene.RemoveNode(targetsNode)
+            continue
+
+        # Set data to Curve Maker module
+        destNode = slicer.mrmlScene.CreateNodeByClass('vtkMRMLModelNode')
+        slicer.mrmlScene.AddNode(destNode)
+        cmlogic.SourceNode = trajectoryNode
+        cmlogic.DestinationNode = destNode
+        cmlogic.enableAutomaticUpdate(True)
+        cmlogic.updateCurve()
+
+        # Calculate targeting error
+        (robotTgtDist, robotTgtOffset) = cmlogic.distanceToPoint(robotTgt, True)
+        print 'TARGETING ERROR: %.3f, (%.3f, %.3f, %.3f)' % (robotTgtDist, robotTgtOffset[0], robotTgtOffset[1], robotTgtOffset[2])
+
+        # Calculate biopsy error
+        (biopsyTgtDist, biopsyTgtOffset) = cmlogic.distanceToPoint(biopsyTgt, True)
+        print 'BIOPSY ERROR: %.3f, (%.3f, %.3f, %.3f)' % (biopsyTgtDist, biopsyTgtOffset[0], biopsyTgtOffset[1], biopsyTgtOffset[2])
+
+        cmlogic.SourceNode = None
+        cmlogic.DestinationNode = None
         slicer.mrmlScene.RemoveNode(transformNode)
         slicer.mrmlScene.RemoveNode(targetsNode)
+        slicer.mrmlScene.RemoveNode(destNode)
+        slicer.mrmlScene.RemoveNode(trajectoryNode)
 
 
 if __name__ ==  "__main__":
